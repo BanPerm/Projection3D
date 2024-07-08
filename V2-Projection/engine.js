5eb4613c71b0d06414d5df6cb546dc719e27945b
Poirrier Peter <peterdesete@gmail.com>
2024-07-08 14:05:42 +0200

    V2 Projection fonctionnelle + rotation à mettre en place

--- V2-Projection/engine.js
+++ V2-Projection/engine.js
@@ -45,10 +45,10 @@ class CubeMesh {
         this.mesh = new Mesh();
     }
 
-    create(base=[0,0,0], longueur=100, largeur=100, profondeur=1) {
-        let x = base[0];
-        let y = base[1];
-        let z = base[2];
+    create(base=new Vector3D(0,0,0), longueur=1, largeur=1, profondeur=1) {
+        let x = base.x;
+        let y = base.y;
+        let z = base.z;
         longueur = x + longueur;
         largeur = y + largeur;
         profondeur = z + profondeur;
@@ -56,23 +56,23 @@ class CubeMesh {
             //Devant
             new Triangle(new Vector3D(x, y, z),new Vector3D(x, largeur, z), new Vector3D(longueur, largeur, z)),
             new Triangle(new Vector3D(x, y, z),new Vector3D(longueur, largeur, z),new Vector3D(longueur, y, z)),
-        
+
             // Derrière
             new Triangle(new Vector3D(longueur, y, profondeur),new Vector3D(longueur, largeur, profondeur),new Vector3D(x, largeur, profondeur)),
             new Triangle(new Vector3D(longueur, y, profondeur),new Vector3D(x, largeur, profondeur),new Vector3D(x, y, profondeur)),
-        
+
             // Gauche
             new Triangle(new Vector3D(longueur, y, z),new Vector3D(longueur, largeur, z),new Vector3D(longueur, largeur, profondeur)),
             new Triangle(new Vector3D(longueur, y, z),new Vector3D(longueur, largeur, profondeur),new Vector3D(longueur, y, profondeur)),
-        
+
             // Droite
             new Triangle(new Vector3D(x, y, profondeur),new Vector3D(x, largeur, profondeur),new Vector3D(x, largeur, z)),
             new Triangle(new Vector3D(x, y, profondeur),new Vector3D(x, largeur, z),new Vector3D(x, y, z) ),
-        
+
             // Haut
             new Triangle(new Vector3D(x, largeur, z),new Vector3D(x, largeur, profondeur),new Vector3D(longueur, largeur, profondeur)),
             new Triangle(new Vector3D(x, largeur, z),new Vector3D(longueur, largeur, profondeur),new Vector3D(longueur, largeur, z)),
-        
+
             // Bas
             new Triangle(new Vector3D(longueur, y, profondeur),new Vector3D(x, y, profondeur),new Vector3D(x, y, z)),
             new Triangle(new Vector3D(longueur, y, profondeur),new Vector3D(x, y, z),new Vector3D(longueur, y, z))
@@ -82,37 +82,39 @@ class CubeMesh {
     }
 
     draw() {
-        //@TODO Manque la projection
         for (let triangle of this.mesh.pos) {
-            drawTriangle(triangle); 
+            drawTriangle(triangle);
         }
     }
 }
 
+function multiplication(matrice, triangle){
+    let o= new Vector3D(0,0,0);
+    o.x = triangle.x * matrice[0][0] + triangle.y * matrice[1][0] + triangle.z * matrice[2][0] + matrice[3][0];
+    o.y = triangle.x * matrice[0][1] + triangle.y * matrice[1][1] + triangle.z * matrice[2][1] + matrice[3][1];
+    o.z = triangle.x * matrice[0][2] + triangle.y * matrice[1][2] + triangle.z * matrice[2][2] + matrice[3][2];
+    let w = triangle.x * matrice[0][3] + triangle.y * matrice[1][3] + triangle.z * matrice[2][3] + matrice[3][3];
 
-//Fonction utile hors projection
-function matrixProduct(m1, m2) {
-    const h = m1.length;
-    const w = m2[0].length;
-    const matrix = Array.from({ length: h }, () => Array(w).fill(0));
-
-    for (let i = 0; i < h; i++) {
-        for (let j = 0; j < w; j++) {
-            for (let k = 0; k < m2.length; k++) {
-                matrix[i][j] += m1[i][k] * m2[k][j];
-            }
-        }
+    if (w !==0.0){
+        o.x /= w; o.y /= w; o.z /=w;
     }
-    return matrix;
+
+    return o;
 }
 
+function drawCube(triangles) {
+    for (let triangle of triangles) {
+        drawTriangle(triangle);
+    }
+}
 
 //Matrice de rotation
 function rotation_x(angle) {
     return [
-        [1, 0, 0],
-        [0, Math.cos(angle), -Math.sin(angle)],
-        [0, Math.sin(angle), Math.cos(angle)]
+        [1, 0, 0, 0],
+        [0, Math.cos(angle), Math.sin(angle),0],
+        [0, -Math.sin(angle), Math.cos(angle),0],
+        [0, 0, 0, 1]
     ];
 }
 
@@ -132,50 +134,56 @@ function rotation_z(angle) {
     ];
 }
 
-function projection(triangle){
-    projectionMatrix = [
+function drawTriangle(triangle) {
+
+    let projectionMatrix = [
         [aspectRatio*fovRad,0,0,0],
         [0,fovRad,0,0],
         [0,0,zfar/(zfar-znear),1],
         [0,0,(-zfar*znear)/(zfar-znear),0]
     ];
 
-    return projectionMatrix;
+    // multiplication(rotation_x(0.6), triangle.pos[0])
+    // multiplication(rotation_x(0.6), triangle.pos[1]);
+    // multiplication(rotation_x(0.6), triangle.pos[2]);
 
-}
+    triangle.pos[0].z += 100;
+    triangle.pos[1].z += 100;
+    triangle.pos[2].z += 100;
 
-function multiplication(matrice, triangle){
-    o.x = triangle.x * matrice[0][0] + triangle.y * matrice[1][0] + triangle.z * matrice[2][0] + matrice[3][0];
-    o.y = triangle.x * matrice[0][1] + triangle.y * matrice[1][1] + triangle.z * matrice[2][1] + matrice[3][1];
-    o.z = triangle.x * matrice[0][2] + triangle.y * matrice[1][2] + triangle.z * matrice[2][2] + matrice[3][2];
-    w = triangle.x * matrice[0][3] + triangle.y * matrice[1][3] + triangle.z * matrice[2][3] + matrice[3][3];
-
-    if (w !=0.0){
-        o.x /= w; o.y /= w; o.z /w;
+    for(let tri of triangle.pos){
+        multiplication(projectionMatrix,tri);
     }
 
-    return o;
-}
-
-function drawCube(triangles) {
-    for (let triangle of triangles) {
-        drawTriangle(triangle);
-    }
-}
-
-function drawTriangle(triangle) {
-    console.log("Triangle: "+triangle.pos[0].x);
     for (let i = 0; i < 3; i++) {
         let p1 = triangle.pos[i];
         let p2 = triangle.pos[(i + 1) % 3];
 
+        ctx.strokeStyle = 'rgba(68,255,0,0.5)';
         ctx.beginPath();
         ctx.moveTo(p1.x, p1.y);
         ctx.lineTo(p2.x, p2.y);
         ctx.stroke();
-
     }
 }
 
 let cube = new CubeMesh;
-cube.create();
+cube.create(new Vector3D(width/2,height/2,0), 100,100,100);
+
+
+
+//Fonction utile hors projection
+function matrixProduct(m1, m2) {
+    const h = m1.length;
+    const w = m2[0].length;
+    const matrix = Array.from({ length: h }, () => Array(w).fill(0));
+
+    for (let i = 0; i < h; i++) {
+        for (let j = 0; j < w; j++) {
+            for (let k = 0; k < m2.length; k++) {
+                matrix[i][j] += m1[i][k] * m2[k][j];
+            }
+        }
+    }
+    return matrix;
+}
